d2e4eae46cb5ec14de4e51dc645fecaa840eb363
Aqeeb H <35127065+aqeebhussain122@users.noreply.github.com>
2019-04-30 12:07:12 +0100

    Update write_file.c

--- write_file.c
+++ write_file.c
@@ -5,24 +5,28 @@
 int main(int argc, char **argv)
 {
    int capacity = 1; // capacity of the buffer for malloc
-   int len = 0; // Length to track the buffers length itself
-   char *buffer = NULL;
+   size_t len = 0; // Length to track the buffers length itself - size_t has heavily decreased the errors
+   char *buffer = NULL; // Working memory space
+   // char **strs 	= NULL; // String pointers to attach 
    // Currently this will be one but will expand 
-   buffer = (char*)malloc(capacity * sizeof(char));
+   buffer = malloc(capacity * sizeof(char));
    FILE *file_ptr = NULL; // 
    // Assigning a pointer to argv[1] for variable storage 
-   assert(argv[1] != NULL);
+   //assert(argv[1] != NULL);
    // argv[1] assigned as the file path
-   file_ptr = fopen(argv[1], "w+");
+   file_ptr = fopen("file", "w"); // Memory leak in assignment to argv[1] - arbitrary filename
    // Takes count of the buffer as while loop 
    while (fgets(buffer, sizeof(buffer), stdin) && buffer != NULL) 
    {
-        // This will count the number of characters in the buffer, printing and reallocing memory as needed  
-        for(len = 0; buffer[len] != '\n'; len++)
+	
+	// Potential unintialised pointer
+        for(len = 0; buffer[len] != '\0' && buffer[len] != EOF; len++)
         {
-                if(++len == capacity)
+                if(len == capacity)
                 {
-                        buffer = realloc(buffer, (capacity *= 2) * sizeof(char));
+                        buffer = realloc(buffer, (capacity *= 2) * sizeof(char)); // memory problem 2
+			// Might cause an error
+			//free(buffer);
                 }
         }
         // Loop through contents of the buffer itself
