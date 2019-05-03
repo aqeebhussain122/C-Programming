c3ff358e7512ec5a67e25e97e6b17b530ba25175
Aqeeb <aqeebrajhussain@gmail.com>
2019-05-03 11:00:58 +0100

    Memory fix

--- network-program/write_file.c
+++ network-program/write_file.c
@@ -5,32 +5,29 @@
 
 int main(int argc, char **argv)
 {
-   //size_t capacity = 1; // capacity of the buffer for malloc
-   // Length to track the buffers length itself - size_t has heavily decreased the errors
-   char *buffer = malloc(1000 * sizeof(char)); // Working space of the file
-   FILE *file_ptr = NULL; // 
+   size_t capacity = 1; // capacity of the buffer for malloc
+   char *buffer = malloc((int)capacity * sizeof(char*)); // Working space of the file
+   //char *file_name = argv[1];
+   FILE *file_ptr = NULL; 
    assert(argv[1] != NULL);
-   // argv[1] assigned as the file path
-   file_ptr = fopen(argv[1], "a"); // Memory leak in assignment to argv[1] - arbitrary filename
+   file_ptr = fopen(argv[1], "a");
    // Takes count of the buffer as while loop
    while (fgets(buffer, sizeof(buffer), stdin) && buffer != NULL)
    {
-	/*for(size_t len = 0; buffer[len] != '\0'; len++)
+        for(size_t len = 0; len <= *buffer; len++)
         {
-                if(++len == capacity)
+                if(len == (size_t)capacity)
                 {
-			// realloc does the buffer 
-                        buffer = realloc(buffer, (capacity *= 2) * sizeof(char*)); // memory problem 2
+                        buffer = realloc(buffer, (capacity *= 2) * sizeof(char*));
                 }
         }
-	*/
-        // Loop through contents of the buffer itself
         fprintf(file_ptr, "%s", buffer);
    }
    // File stream closed and buffer memory is freed
    fclose(file_ptr);
    free(buffer);
    buffer = NULL;
+   file_ptr = NULL;
    return 0;
 
 // https://stackoverflow.com/questions/8680220/how-to-get-the-value-of-individual-bytes-of-a-variable
